3cb435926556c23008de1335be87fb3f01895587
Ayan <ayanparkar123@gmail.com>
2023-09-13 22:40:40 +0530

    fix: env.mjs

--- env.mjs
+++ env.mjs
@@ -1,17 +1,19 @@
-import { z } from "zod";
+import { z } from "zod"
 
 const server = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
 
-  NODE_ENV: z.enum(["development", "test", "production"]),
-});
+    NODE_ENV: z.enum(["development", "test", "production"]),
+    NEXT_PUBLIC_SITE_URL: z.preprocess(
+        (str) => process.env.VERCEL_URL ?? str,
+        process.env.VERCEL ? z.string().min(1) : z.string().url()
+    ),
+})
 
 const client = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-  // client variables
-});
+    // client variables
+})
 
 /**
  * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -21,50 +23,51 @@ const client = z.object({
  */
 
 const processEnv = {
-  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  NODE_ENV: process.env.NODE_ENV,
-};
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+}
 
-const merged = server.merge(client);
+const merged = server.merge(client)
 
 /** @typedef {z.input<typeof merged>} MergedInput */
 /** @typedef {z.infer<typeof merged>} MergedOutput */
 /** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
 
-let env = /** @type {MergedOutput} */ (process.env);
+let env = /** @type {MergedOutput} */ (process.env)
 
 if (!!process.env.SKIP_ENV_VALIDATION == false) {
-  const isServer = typeof window === "undefined";
+    const isServer = typeof window === "undefined"
 
-  const parsed = /** @type {MergedSafeParseReturn} */ (
-    isServer
-      ? merged.safeParse(processEnv) // on server we can validate all env vars
-      : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  );
+    const parsed = /** @type {MergedSafeParseReturn} */ (
+        isServer
+            ? merged.safeParse(processEnv) // on server we can validate all env vars
+            : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
+    )
 
-  if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
-    );
-    throw new Error("Invalid environment variables");
-  }
+    if (parsed.success === false) {
+        console.error(
+            "❌ Invalid environment variables:",
+            parsed.error.flatten().fieldErrors
+        )
+        throw new Error("Invalid environment variables")
+    }
 
-  env = new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined;
-      // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-        throw new Error(
-          process.env.NODE_ENV === "production"
-            ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
-        );
-      return target[/** @type {keyof typeof target} */ (prop)];
-    },
-  });
+    env = new Proxy(parsed.data, {
+        get(target, prop) {
+            if (typeof prop !== "string") return undefined
+            // Throw a descriptive error if a server-side env var is accessed on the client
+            // Otherwise it would just be returning `undefined` and be annoying to debug
+            if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+                throw new Error(
+                    process.env.NODE_ENV === "production"
+                        ? "❌ Attempted to access a server-side environment variable on the client"
+                        : `❌ Attempted to access server-side environment variable '${prop}' on the client`
+                )
+            return target[/** @type {keyof typeof target} */ (prop)]
+        },
+    })
 }
 
-export { env };
+export { env }
