3a28d796da8f430b2ee86372396a6dd0382101ee
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-09-13 23:46:30 +0530

    added middleware checks

--- middleware.ts
+++ middleware.ts
@@ -7,51 +7,38 @@ export async function middleware(req: NextRequest) {
     // Create a Supabase client configured to use cookies
     const supabase = createMiddlewareClient({ req, res })
 
-    async function checkIfIdExistsInTable(
-        tableName: string,
-        idToCheck: string
-    ): Promise<boolean> {
-        const { data, error } = await supabase
-            .from(tableName)
-            .select("*")
-            .eq("id", idToCheck)
-
-        if (error) {
-            throw error
-        }
-
-        // Check if any rows were returned
-        return data.length > 0
-    }
-
     // Refresh session if expired - required for Server Components
     // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
     const session = await supabase.auth.getSession()
     if (session.data.session !== null) {
         const user = session.data.session.user
-
-        checkIfIdExistsInTable("student", user.id)
-            .then((exists) => {
-                if (exists) {
-                    console.log(`already registered`)
-                } else {
-                    const { pathname } = req.nextUrl
-
-                    if (pathname.startsWith("/_next"))
-                        return NextResponse.next()
-
-                    req.nextUrl.pathname = "/welcome"
-                    return NextResponse.redirect(req.nextUrl)
-                }
-            })
-            .catch((error) => {
-                console.error("Error checking ID:", error)
-            })
+        console.log(user)
+        // if (req.nextUrl.pathname.startsWith("/_next")) {
+        //     return NextResponse.next()
+        // }
+        // if (req.nextUrl.pathname.startsWith("/?code")) {
+        //     return NextResponse.next()
+        // }
+        console.log(req.url)
+        const { data, error } = await supabase
+            .from("student")
+            .select("*")
+            .eq("id", user.id)
+        if (error) {
+            throw error
+        }
+        console.log(data.length > 0)
+        console.log(data)
+        if (data.length > 0) {
+            console.log("existing.. ")
+        } else {
+            return NextResponse.redirect(new URL("/welcome", req.url))
+        }
     }
 
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: "/welcome",
+    matcher: ["/"],
 }
