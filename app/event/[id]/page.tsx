6d0beee2e813e64b53626823b400d805a6ec4dc3
Ayan <ayanparkar123@gmail.com>
2023-09-29 22:19:04 +0530

    feat: added leader verification in server side

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -7,6 +7,7 @@ export default async function EventPage({ params }: { params: any }) {
     const {
         data: { session },
     } = await supabase.auth.getSession()
+
     const { data: event } = await supabase
         .from("event")
         .select("*")
@@ -14,11 +15,16 @@ export default async function EventPage({ params }: { params: any }) {
         .eq("isopen", true)
         .single()
 
-    const { data: user, error } = await supabase
+    const { data: user, error: userError } = await supabase
         .from("student")
         .select("enrollment")
         .eq("id", session?.user.id ?? "")
         .single()
+
+    const { data: groups, error: groupError } = await supabase
+        .from("eventparticipant")
+        .select(`group!inner(id, groupmember!inner(student_id))`)
+        .eq("group.groupmember.student_id", session?.user.id ?? "")
 
     if (!session || !user) {
         return (
@@ -30,7 +36,9 @@ export default async function EventPage({ params }: { params: any }) {
                 </p>
             </div>
         )
-    } else if (error || !event) {
+    }
+
+    if (userError || groupError || !event) {
         return (
             <div className="flex flex-col gap-2 justify-center items-center">
                 <h1 className="text-3xl font-bold">
@@ -40,7 +48,20 @@ export default async function EventPage({ params }: { params: any }) {
                     Sorry for the inconvienience. Try again later, if the same
                     error occurs then kindly contact the admins!
                 </p>
-                <pre>{error}</pre>
+                {userError && <pre>{userError}</pre>}
+                {groupError && <pre>{groupError.message}</pre>}
+            </div>
+        )
+    }
+
+    if (groups && groups?.length > 0) {
+        return (
+            <div className="flex flex-col gap-2 justify-center items-center">
+                <h1 className="text-3xl font-bold">Already Registered</h1>
+                <p>
+                    You have already registered yourself in this event, kindly
+                    contact the admins if you want to remove yourself.
+                </p>
             </div>
         )
     }
