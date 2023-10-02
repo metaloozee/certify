6b85d049a892d14525a0afcc5a9352de65b7887b
Ayan <ayanparkar123@gmail.com>
2023-10-02 21:19:39 +0530

    fix: minor bugs

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 
-import type { Database } from "@/types/supabase"
+import { Database } from "@/types/supabase"
+
+export const dynamic = "force-dynamic"
 
 export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url)
