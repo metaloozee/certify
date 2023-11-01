09f8455bf067d51efc74d72631d21aa764574444
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-11-01 17:11:21 +0530

    workflow test

--- components/template-canvas.tsx
+++ components/template-canvas.tsx
@@ -113,7 +113,7 @@ export const CanvasImage = ({
                 type="file"
                 id="template"
             />
-
+            workflow test, please remove if you see this
             <div>
                 <RadioGroup className="flex mt-5" defaultValue="name">
                     <div className="flex items-center space-x-2">
