909b35511e5ab23fa5d5c80ef841b58455651e83
Ayan <ayanparkar123@gmail.com>
2023-11-15 20:09:21 +0530

    fix: ui & already registered bug

--- app/page.tsx
+++ app/page.tsx
@@ -10,42 +10,14 @@ export default async function Index() {
 
     const { data } = await supabase.from("event").select("*").eq("isopen", true)
 
-    const eventdata = await supabase.from("eventparticipant").select(`
-        event_id,
-            group (
-                groupmember (
-                    student (
-                        id
-                    )
-                )
-            )
-        `)
-
-    const session = (await supabase.auth.getSession()).data.session
-    const checkIfPartcipated = (id: string) => {
-        let status = false
-        eventdata.data?.forEach((d: any) => {
-            if (d.event_id === id) {
-                d.group.groupmember.forEach((member: any) => {
-                    if ((member.student.id = session?.user.id)) {
-                        status = true
-                    }
-                })
-            }
-        })
-        return status
-    }
-
     return data && data.length > 0 ? (
         <div className="flex flex-col justify-center items-center gap-10">
-            <h1 className="text-3xl font-bold">Upcoming Competetions</h1>
-            <div className="flex flex-wrap justify-between items-center gap-5">
+            <h1 className="text-center text-lg md:text-3xl font-bold">
+                Upcoming Competetions
+            </h1>
+            <div className="container grid grid-cols-1 md:grid-cols-3 gap-5">
                 {data.reverse().map((d) => (
-                    <EventCard
-                        key={d.id}
-                        data={d}
-                        status={checkIfPartcipated(d.id)}
-                    />
+                    <EventCard key={d.id} data={d} />
                 ))}
             </div>
         </div>
--- components/event-card.tsx
+++ components/event-card.tsx
@@ -10,6 +10,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { createServerSupabaseClient } from "@/app/supabase-server"
 
 export type EventData = {
     id: string
@@ -20,13 +21,23 @@ export type EventData = {
     team_limit: number
 }
 
-export const EventCard = async ({
-    data,
-    status,
-}: {
-    data: EventData
-    status: boolean
-}) => {
+export const EventCard = async ({ data }: { data: EventData }) => {
+    const supabase = await createServerSupabaseClient()
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+    const { data: event } = await supabase
+        .from("event")
+        .select("*")
+        .eq("id", data.id)
+        .eq("isopen", true)
+        .single()
+    const { data: groups, error: groupError } = await supabase
+        .from("eventparticipant")
+        .select(`group!inner(id, groupmember!inner(student_id))`)
+        .eq("group.groupmember.student_id", session?.user.id ?? "")
+        .eq("event_id", event?.id ?? "")
+
     let eventDate = new Date(data.date ?? "")
     let now = new Date()
     let timeRemaining = eventDate.getTime() - now.getTime()
@@ -37,27 +48,30 @@ export const EventCard = async ({
     )
 
     return (
-        <Card className="max-w-md">
-            <CardHeader>
-                <CardTitle>{data.name}</CardTitle>
-                <CardDescription>{data.description}</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-                <Badge>MAX MEMBERS: {data.team_limit}</Badge>
-                <p className="text-sm text-green-500">
-                    {`Starts in: ${days} days, ${hours} hours` ?? "loading.."}
-                </p>
-            </CardContent>
-            <CardFooter>
-                {status === false ? (
+        <Card className="w-full h-full flex flex-col justify-between">
+            <div>
+                <CardHeader>
+                    <CardTitle>{data.name}</CardTitle>
+                    <CardDescription>{data.description}</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                    <Badge>MAX MEMBERS: {data.team_limit}</Badge>
+                    <p className="text-sm text-green-500">
+                        {`Starts in: ${days} days, ${hours} hours` ??
+                            "loading.."}
+                    </p>
+                </CardContent>
+            </div>
+            <CardFooter className="justify-end">
+                {groups && groups?.length > 0 ? (
+                    <Button className="w-full" disabled>
+                        Already Registered
+                    </Button>
+                ) : (
                     <Button className="w-full">
                         <Link className="w-full" href={`/event/${data.id}`}>
                             Register
                         </Link>
-                    </Button>
-                ) : (
-                    <Button disabled className="w-full">
-                        Already participated
                     </Button>
                 )}
             </CardFooter>
