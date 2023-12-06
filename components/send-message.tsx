b57ab6ba71de47d781004bf39fb6d46fb0ebbd79
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-12-06 15:09:50 +0530

    fix: message form (footer)

---
+++ components/send-message.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Session } from "@supabase/supabase-js"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/use-toast"
+import { useSupabase } from "@/app/supabase-provider"
+
+const SendMessage = ({ session }: { session: Session | null }) => {
+    const { supabase } = useSupabase()
+
+    const [message, setMessage] = useState<string>("")
+
+    const handleSubmit = () => {
+        if (session) {
+            supabase
+                .from("messages")
+                .insert({
+                    from: session.user.id,
+                    content: message,
+                })
+                .then((res) => {
+                    toast({
+                        title: "Message sent successfully!",
+                        description:
+                            "Admin will look up your message and contact you.",
+                    })
+                    setMessage("")
+                })
+        } else {
+            return toast({
+                title: "Please login first...",
+                variant: "destructive",
+            })
+        }
+    }
+
+    return (
+        <div className="flex gap-2 mt-2">
+            <Input
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Please remove me from Solo quiz competition..."
+            />
+            <Button onClick={handleSubmit}>Submit</Button>
+        </div>
+    )
+}
+
+export default SendMessage
